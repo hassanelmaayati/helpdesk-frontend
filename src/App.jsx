a74755e87cd954d99550f332d2c16c46a270c3b8
LockVision <hassanelmaayati@gmail.com>
2026-09-14 11:06:50 +0300

    Set up React Router routes for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import EmployeeDashboard from './pages/EmployeeDashboard';
+import ITStaffDashboard from './pages/ITStaffDashboard';
+import TicketForm from './pages/TicketForm';
+import TicketDetail from './pages/TicketDetail';
+import ManageCategories from './pages/ManageCategories';
+
 function App() {
   return (
-    <div>
-      <h1>HelpDesk</h1>
-    </div>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/employee-dashboard" element={<EmployeeDashboard />} />
+      <Route path="/it-dashboard" element={<ITStaffDashboard />} />
+      <Route path="/tickets/new" element={<TicketForm />} />
+      <Route path="/tickets/:id/edit" element={<TicketForm />} />
+      <Route path="/tickets/:id" element={<TicketDetail />} />
+      <Route path="/categories" element={<ManageCategories />} />
+    </Routes>
   );
 }
 
