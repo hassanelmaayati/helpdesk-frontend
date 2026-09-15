5189157dddffc82593ef2ff31620d64ca0aa6241
Maram Shubbar <maram20052@gmail.com>
2026-09-15 11:45:09 +0300

    done

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,6 +1,5 @@
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../context/useAuth';
-
+import { useAuth } from "../../context/useAuth";
 function ProtectedRoute({ children }) {
   const { user } = useAuth();
   if (!user) return <Navigate to="/login" />;
