1d4699c3177fe042984468a48d48979f3de409ce
LockVision <hassanelmaayati@gmail.com>
2026-09-15 09:36:12 +0300

    .

--- src/components/ITStaffDashboard.jsx
+++ src/components/ITStaffDashboard.jsx
@@ -1,40 +1,46 @@
-import { useState, useEffect } from 'react'
-import StatCards from './StatCards'
-import TicketList from './TicketList'
+import { useState, useEffect } from "react";
+import StatCards from "./StatCards";
+import TicketList from "./TicketList";
 
-const [tickets, setTickets] = useState([])
+const [tickets, setTickets] = useState([]);
 
-function ITStaffDashboard(){
-    useEffect(() => {
-  fetch('http://localhost:3001/tickets')
-    .then((response) => response.json())
-    .then((data) => {
-      setTickets(data)
-    })
-}, [])
-    const open = tickets.filter((ticket) => ticket.status === 'Open').length
-    const inProgress = tickets.filter((ticket) => ticket.status === 'In Progress').length
-    const Resolved = tickets.filter((ticket) => ticket.status === 'Resolved').length
-    const [statusFilter, setStatusFilter] = useState('All')
+function ITStaffDashboard() {
+  useEffect(() => {
+    fetch("http://localhost:3001/tickets")
+      .then((response) => response.json())
+      .then((data) => {
+        setTickets(data);
+      });
+  }, []);
+  const open = tickets.filter((ticket) => ticket.status === "Open").length;
+  const inProgress = tickets.filter(
+    (ticket) => ticket.status === "In Progress",
+  ).length;
+  const Resolved = tickets.filter(
+    (ticket) => ticket.status === "Resolved",
+  ).length;
+  const [statusFilter, setStatusFilter] = useState("All");
 
-    const filteredTickets = statusFilter === 'All'
-  ? tickets
-  : tickets.filter((ticket) => ticket.status === statusFilter)
+  const filteredTickets =
+    statusFilter === "All"
+      ? tickets
+      : tickets.filter((ticket) => ticket.status === statusFilter);
   return (
-  <div>
-    <h2>IT Staff Dashboard</h2>
-    <StatCards open={open} inProgress={inProgress} resolved={resolved} />
-    <select value={statusFilter} onChange={(event) => setStatusFilter(event.target.value)}>
-  <option value="All">All</option>
-  <option value="Open">Open</option>
-  <option value="In Progress">In Progress</option>
-  <option value="Resolved">Resolved</option>
-</select>
-     <TicketList tickets={filteredTickets} />
-
-  </div>
-)
-
+    <div>
+      <h2>IT Staff Dashboard</h2>
+      <StatCards open={open} inProgress={inProgress} resolved={resolved} />
+      <select
+        value={statusFilter}
+        onChange={(event) => setStatusFilter(event.target.value)}
+      >
+        <option value="All">All</option>
+        <option value="Open">Open</option>
+        <option value="In Progress">In Progress</option>
+        <option value="Resolved">Resolved</option>
+      </select>
+      <TicketList tickets={filteredTickets} />
+    </div>
+  );
 }
 
 export default ITStaffDashboard;
